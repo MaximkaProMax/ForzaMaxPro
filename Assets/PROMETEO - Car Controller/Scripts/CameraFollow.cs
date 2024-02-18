05599525e79f3334720b3c051c76e17de310f7c3
Wayrent <sebu1404@gmail.com>
2024-02-18 19:14:03 +0300

    Camera Fix

--- Assets/PROMETEO - Car Controller/Scripts/CameraFollow.cs
+++ Assets/PROMETEO - Car Controller/Scripts/CameraFollow.cs
@@ -9,6 +9,53 @@ public class CameraFollow : MonoBehaviour {
 	public float followSpeed = 2;
 	[Range(1, 10)]
 	public float lookSpeed = 5;
+	private float distance;
+	private float height;
+
+	void Start(){
+		distance = Vector3.Distance(transform.position, carTransform.position);
+		height = transform.position.y;
+	}
+
+	void FixedUpdate()
+	{
+		//Move to car
+		Vector3 targetPos = carTransform.position - carTransform.forward * distance;
+		targetPos.y = carTransform.position.y + height;
+		transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
+
+		//Look at car
+		Vector3 lookDirection = carTransform.position - transform.position;
+		Quaternion rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+		float clampedZ = Mathf.Clamp(rotation.eulerAngles.z, -1f, 1f); // clamp the z rotation between -1 and 1
+		rotation = Quaternion.Euler(5f, rotation.eulerAngles.y, clampedZ);
+		transform.rotation = Quaternion.Lerp(transform.rotation, rotation, lookSpeed * Time.deltaTime);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* using System.Collections; // СТАРЫЙ СКРИПТ НА ВСЯКИЙ СЛУЧАЙ
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform carTransform;
+	[Range(1, 10)]
+	public float followSpeed = 2;
+	[Range(1, 10)]
+	public float lookSpeed = 5;
 	Vector3 initialCameraPosition;
 	Vector3 initialCarPosition;
 	Vector3 absoluteInitCameraPosition;
@@ -32,4 +79,4 @@ public class CameraFollow : MonoBehaviour {
 
 	}
 
-}
+} */
